aadeb13d897bc2154f8eeddc9534816f9c3f31bb
sanjeet kumar Thakur <sanjeetkumarthakur911@gmail.com>
2025-09-27 09:23:20 +0545

    onclick

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
+ import React, { useState, useRef } from "react";
+import "./Quiz.css";
+import { data } from "../assets/data";
 
 const Quiz = () => {
-  return (
-    
-      <div className='container'>
-    <h1> Quiz App</h1>
-    <hr />
-    <h2>Which device is reqired for the Internet connection?</h2>
-    <ul>
-     <li>Modem</li>  
-      <li>Router</li>
-       <li>LAN Cable</li>
-        <li>Pen Drive</li> 
-    </ul>
-    <button>Next</button>
-    <div className='index'>1 to 5 questions</div>
-      </div>
-    
-  )
-}
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+  let [lock, setLock] = useState(false);
+ let option1 = useRef(null);
+  let option2= useRef(null);
 
-export default Quiz
+   let option3= useRef(null);
+
+    let option4 = useRef(null);
+    let option_array =[option1,option2,option3,option4];
+
+  const checkAns = (e, ans) => {
+    if (lock === false) {
+      if (question.ans === ans) {
+        e.target.classList.add("correct");
+        setLock(true);
+      } else {
+        e.target.classList.add("wrong");
+        setLock(true);
+        option_array[question.ans-1].current.classList.add("correct");
+      }
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
+
+      <ul>
+        <li ref={option1} onClick={(e) => checkAns(e, 1)}>{question.option1}</li>
+        <li ref={option2} onClick={(e) => checkAns(e, 2)}>{question.option2}</li>
+        <li  ref={option3} onClick={(e) => checkAns(e, 3)}>{question.option3}</li>
+        <li ref={option4} onClick={(e) => checkAns(e, 4)}>{question.option4}</li>
+      </ul>
+
+      <button>Next</button>
+      <div className="index">{index + 1} of {data.length} questions</div>
+    </div>
+  );
+};
+
+export default Quiz;
